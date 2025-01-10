045927b254480a78640b903282771cd0f8882842
pratyush vashisht <pratyush.vashisht@brightlifecare.com>
2025-01-10 11:45:19 +0530

    springboot setup

---
+++ flinkwebapp/src/main/java/com/example/flinkwebapp/FlinkwebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.flinkwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlinkwebappApplication.class, args);
+	}
+
+}
